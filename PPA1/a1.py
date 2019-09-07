c75efc99c2e24f0bbfc3beca6305d72c903286ab
Alain Garcia <alaingarcia@ufl.edu>
2019-09-07 17:08:38 -0400

    Added some more comments and changed email function to isValidEmail
    - email used to return 'Invalid' or 'Valid'. isValidEmail now returns True or False
    - added some comments to clarify what functions did again

--- PPA1/a1.py
+++ PPA1/a1.py
@@ -3,19 +3,16 @@ import math
 import re
 
 # BODY MASS INDEX FUNCTION
-# Input: heightInput (in feet and inches, ex: 5'10)
-# Input: weightInput (in pounds, ex: 180)
+# Input: height_input (in feet and inches, ex: 5'10")
+# Input: weight_input (in pounds, ex: 180)
 # Output: BMI, WeightClassification
 def bmi(height_input, weight_input):
-    if ('\'' not in height_input):
-        print('Incorrect input for height detected.' +
-        '\nAs an example, if you are 5 feet and 10 inches tall, enter: 5\'10')
     
     # Seperate into feet and inches by splitting the string by the single quote
     ft, inch = height_input.split('\'')
 
-    # Convert to floats
-    ft, inch = float(ft), float(inch)
+    # Convert to floats (remove " in inches if there is one)
+    ft, inch = float(ft), float(re.sub('\"', '', inch))
 
     # Turns the height into inches
     height = ft*12 + inch
@@ -44,12 +41,12 @@ def bmi(height_input, weight_input):
 # Input: savings_goal (Savings goal before death, ex: $500,000)
 # Output: savings_goal_age (Age at which the savings_goal is achieved)
 def retirement(current_age, salary, saved_percent, savings_goal):
-
+    
     # Removes special characters
     def removeSpecial(string):
         return re.sub('[!@#$%^&*,]', '', string)
     
-    # Conversion from string to float
+    # Remove special characters for all inputs
     salary = float(removeSpecial(salary))
     saved_decimal = float(removeSpecial(saved_percent)) / 100
     savings_goal = float(removeSpecial(savings_goal))
@@ -61,7 +58,14 @@ def retirement(current_age, salary, saved_percent, savings_goal):
     savings_goal_age = years_to_goal + current_age
     return savings_goal_age
 
+# SHORTEST DISTANCE FUNCTION
+# Input: x1 (x coordinate of point 1)
+# Input: y1 (y coordinate of point 1)
+# Input: x2 (x coordinate of point 2)
+# Input: y2 (y coordinate of point 2)
+# Output: distance (euclidian distance between point 1 and 2)
 def shortestDistance(x1, y1, x2, y2):
+    
     x_squared = (x2 - x1) * (x2 - x1)
     y_squared = (y2 - y1) * (y2 - y1)
     squared_distance = x_squared + y_squared
@@ -69,38 +73,44 @@ def shortestDistance(x1, y1, x2, y2):
 
     return distance
 
-def email(email_string):
+# EMAIL VERIFICATION FUNCTION
+# Input: email_string (string which has email to be verified)
+# Output: True/False (True if valid, False if invalid)
+def isValidEmail(email_string):
+    
     # needs at least one @, but more than one @ is improper formatting
     if email_string.count('@') != 1:
-        return('Invalid')
+        return False
 
+    # split email into some_string '@' domain
     some_string, domain = email_string.split('@')
 
     # some_string/domain has to be at least 1 character
     if (len(some_string) == 0) or (len(domain) == 0):
-        return('Invalid')
+        return False
 
     # some_string can't start or end with '.'
     if some_string[0] == '.' or some_string[-1] == '.':
-        return('Invalid')
+        return False
 
     # no consecutive periods
     if '..' in some_string:
-        return('Invalid')
+        return False
 
     # must start with a non-numeric
     if some_string[0].isnumeric():
-        return('Invalid')
+        return False
     
     # can't contain "(),:;<>@[\]'
     for char in '\"(),:;<>@[\\]\'':
         if char in some_string:
-            return('Invalid')
+            return False
     
     # if nothing is wrong, it's valid
-    return('Valid')
+    return True
     
 def cliInterface():
+    
     print('____________________')
     print('  Select a function\n')
 
@@ -115,7 +125,7 @@ def cliInterface():
 
     if(function == 1):
         print('Body Mass Index function selected.')
-        print('Input height in feet and inches. (ex. 5\'10)')
+        print('Input height in feet and inches. (ex. 5\'10\")')
         height_input = input()
 
         print('Input your weight in pounds')
@@ -139,7 +149,6 @@ def cliInterface():
         savings_goal = input()
 
         savings_goal_age = retirement(current_age, salary, saved, savings_goal)
-        
         if(savings_goal_age < 100):
             print('You will reach your savings goal at age', savings_goal_age)
         else:
@@ -153,7 +162,7 @@ def cliInterface():
         y1 = float(input("y1: "))
         x2 = float(input("x2: "))
         y2 = float(input("y2: "))
-
+        
         distance = shortestDistance(x1, y1, x2, y2)
         print('Distance between', (x1,y1), 'and' , (x2,y2), 'is', distance)
 
--- PPA1/a1test.py
+++ PPA1/a1test.py
@@ -1,9 +1,9 @@
-from a1 import bmi, retirement, shortestDistance, email
+from a1 import bmi, retirement, shortestDistance, isValidEmail
 
 def test_bmi():
     # Test confirming BMI calculation from:
     # http://extoxnet.orst.edu/faqs/dietcancer/web2/twohowto.html
-    assert bmi('5\'3', '125') == (22.7, 'Normal weight')
+    assert bmi('5\'3\"', '125') == (22.7, 'Normal weight')
 
 def test_retirement():
     assert retirement(25, '$100,000', '10%', '$1,000,000') == 100
@@ -17,26 +17,26 @@ def test_email():
 
     # Testing proper formatting
     # Needs @, and needs input for some_string and domain
-    assert email('test') == 'Invalid'
-    assert email('@email.com') == 'Invalid'
-    assert email('test@') == 'Invalid'
+    assert isValidEmail('test') == False
+    assert isValidEmail('@email.com') == False
+    assert isValidEmail('test@') == False
 
     # Testing that the email can be separated by periods
-    assert email('test.test.test@email.com') == 'Valid'
+    assert isValidEmail('test.test.test@email.com') == True
 
     # Testing that no periods can start or end some_string
-    assert email('.test@email.com') == 'Invalid'
-    assert email('test.@email.com') == 'Invalid'
+    assert isValidEmail('.test@email.com') == False
+    assert isValidEmail('test.@email.com') == False
 
     # Testing that some_string can not have 2 periods in a row
-    assert email('te..st@email.com') == 'Invalid'
+    assert isValidEmail('te..st@email.com') == False
 
     # Testing that some_string starts with a non-numeric character
-    assert email('1test@email.com') == 'Invalid'
+    assert isValidEmail('1test@email.com') == False
 
     # Test that some_string can contain certain special characters: !$%*+-=?^_{|}~
-    assert email('!$%*+-=?^_{|}~@email.com') == 'Valid'
+    assert isValidEmail('!$%*+-=?^_{|}~@email.com') == True
 
     # Test that some_string can NOT contain certain special characters: "(),:;<>@[\]'
     for char in '\"(),:;<>@[\\]\'':
-        assert email(char+'@email.com') == 'Invalid'
+        assert isValidEmail(char+'@email.com') == False
