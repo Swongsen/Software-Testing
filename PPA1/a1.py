343d70c8b60f9000f7c1641abed5c897d9d391c5
Alain Garcia <alaingarcia@ufl.edu>
2019-09-07 13:29:07 -0400

    Made everything a function
    - Need functions to be able to test
    - Desired functions are now called by CMD app after input is taken in

--- PPA1/a1.py
+++ PPA1/a1.py
@@ -1,6 +1,136 @@
 import sys
 import math
 
+# BODY MASS INDEX FUNCTION
+# I
+def bmi(heightInput, weightInput):
+
+    ft = int(heightinput[0])
+    # If height is say, 5'9 for example.
+    if(len(heightinput) == 3):
+        inch = int(heightinput[2])
+    # If height is say, 5'10 for example.
+    elif(len(heightinput) == 4):
+        # Concatenates the last two characters in the input and converts to an int
+        concat = heightinput[2] + heightinput[3]
+        inch = int(concat)
+
+    # Turns the height into inches
+    height = ft*12 + inch
+    #print("ur height: ", height)
+
+    # Get the weight into kilograms
+    convweight = int(weightinput) * 0.45
+    # Get the height into meters
+    convheight = height * 0.025
+    squared = convheight * convheight
+    BMI = convweight / squared
+
+    # Checking for different BMI ranges
+    if(BMI < 18.5):
+        print("Your BMI is", format(BMI,'.1f'), "and you are underweight.")
+    elif(BMI >= 18.5 and BMI < 25):
+        print("Your BMI is", format(BMI,'.1f'), "and you are normal weight.")
+    elif(BMI >= 25 and BMI < 30):
+        print("Your BMI is", format(BMI,'.1f'), "and you are overweight.")
+    elif(BMI >= 30):
+        print("Your BMI is", format(BMI,'.1f'), "and you are obese.")
+
+def retirement(age, salaryInput, savedInput, savingsGoalInput):
+    # Converts the annual salary string into a float
+    salarystr = ''
+    for x in range(1, len(salaryInput)):
+        salarystr += salaryInput[x]
+    salary = float(salarystr.replace(',',''))
+    #print(salary)
+
+    # Converts the saved percentage string into a float.
+    savedstr = ''
+    for x in range(0, len(savedInput) - 1):
+        savedstr += savedInput[x]
+    saved = float(savedstr.replace('.', ''))
+    savedpercent = saved / 100
+
+    # Converts the desired savings goal string into a float
+    savingsgoalstr = ''
+    for x in range(1, len(savingsGoalInput)):
+        savingsgoalstr += savingsGoalInput[x]
+    savingsgoal = float(savingsgoalstr.replace(',',''))
+    #print(savingsgoal)
+
+    # Calculation for meeting savings goal
+    savingsperyear = salary * savedpercent
+    employermatch = 0.35 * savingsperyear
+    years = 1
+    savings = savingsperyear + employermatch
+
+    while savings <= savingsgoal:
+        savings += savingsperyear + employermatch
+        years += 1
+
+    savingsgoalage = years + age
+
+    if(savingsgoalage < 100):
+        print("You will reach your savings goal at age", savingsgoalage)
+    else:
+        print("You will reach your savings goal after 100 and will not meet it.")
+
+def shortestDistance(x1, y1, x2, y2):
+    xsquared = (x2 - x1) * (x2 - x1)
+    ysquared = (y2 - y1) * (y2 - y1)
+    inside = xsquared + ysquared
+    distance = math.sqrt(inside)
+
+    print("Distance between", (x1,y1), "and" , (x2,y2), "is", distance)
+
+def email(emailstring):
+    # Checks if the first character of the email is an alphabet
+    z = 1
+    if(emailstring[0].isalpha() == False):
+        z = 0
+
+    # Checks if there is an instance of two periods next to each other
+    for x in range(0, len(emailstring) - 1):
+        if(emailstring[x] == '.'):
+            if(emailstring[x+1] == '.'):
+                z = 0
+
+    # Checks for forbidden characters in the email string
+    if('""' in emailstring) or ('(' in emailstring) or (')' in emailstring) or (',' in emailstring) or (':' in emailstring) or (';' in emailstring) or ('<' in emailstring) or ('>' in emailstring) or ('[' in emailstring) or (']' in emailstring) or ('`' in emailstring) or ('\\' in emailstring):
+        z = 0
+    if(emailstring.count('@') > 1 or emailstring.count('@') == 0):
+        z = 0
+
+    # Checks to see if the '@' used in the string is for the @something.com
+    if('@' in emailstring):
+            # If after the @, there is no domain ( @.com)
+            if((len(emailstring) - emailstring.index('@')) == 5):
+                z = 0
+            # If after the @, there is an non-alphabetical letter
+            if(emailstring[emailstring.index('@') + 1].isalpha() == False):
+                z = 0
+            #print(x,"char:", emailstring[x])
+            if(emailstring[len(emailstring) - 1] != 'm'):
+                z = 0
+            if(emailstring[len(emailstring) - 2] != 'o'):
+                z = 0
+            if(emailstring[len(emailstring) - 3] != 'c'):
+                z = 0
+            if(emailstring[len(emailstring) - 4] != '.'):
+                z = 0
+            # If all the previous tests pass, it checks to make sure the domain are letters
+            #if(z == 1):
+            #    for x in range(emailstring.index('@')+1, emailstring.index('.')):
+                    #print(emailstring[x])
+            #        if(emailstring[x].isalpha() == False):
+            #            z = 0
+
+    # If all the tests pass, print out valid email
+    if(z == 1):
+        print("\nCongratulations, you have a valid email address!")
+    elif(z == 0):
+        print("\nInvalid email.")
+
 while True:
     print("____________________")
     print("  Select a function\n")
@@ -13,160 +143,47 @@ while True:
     print("____________________")
 
     function = int(input())
+
     if(function == 1):
         print("Body Mass Index function selected.")
         print("Input height in feet and inches. (ex. 5'10)")
         heightinput = input()
 
-        ft = int(heightinput[0])
-        # If height is say, 5'9 for example.
-        if(len(heightinput) == 3):
-            inch = int(heightinput[2])
-        # If height is say, 5'10 for example.
-        elif(len(heightinput) == 4):
-            # Concatenates the last two characters in the input and converts to an int
-            concat = heightinput[2] + heightinput[3]
-            inch = int(concat)
-
-        # Turns the height into inches
-        height = ft*12 + inch
-        #print("ur height: ", height)
-
         print("Input your weight in pounds")
         weightinput = input()
 
-        # Get the weight into kilograms
-        convweight = int(weightinput) * 0.45
-        # Get the height into meters
-        convheight = height * 0.025
-        squared = convheight * convheight
-        BMI = convweight / squared
-
-        # Checking for different BMI ranges
-        if(BMI < 18.5):
-            print("Your BMI is", format(BMI,'.1f'), "and you are underweight.")
-        elif(BMI >= 18.5 and BMI < 25):
-            print("Your BMI is", format(BMI,'.1f'), "and you are normal weight.")
-        elif(BMI >= 25 and BMI < 30):
-            print("Your BMI is", format(BMI,'.1f'), "and you are overweight.")
-        elif(BMI >= 30):
-            print("Your BMI is", format(BMI,'.1f'), "and you are obese.")
+        bmi(heightinput, weightinput)
 
     elif(function == 2):
         print("Retirement function selected.")
         print("Enter your age")
         age = int(input())
-
-        # Converts the annual salary string into a float
+        
         print("Enter your annual salary. (ex. $60,000)")
-        salaryinput = input()
-        salarystr = ''
-        for x in range(1, len(salaryinput)):
-            salarystr += salaryinput[x]
-        salary = float(salarystr.replace(',',''))
-        #print(salary)
+        salaryInput = input()
 
-        # Converts the saved percentage string into a float.
         print("Enter percentage saved. (ex. 5%)")
-        savedinput = input()
-        savedstr = ''
-        for x in range(0, len(savedinput) - 1):
-            savedstr += savedinput[x]
-        saved = float(savedstr.replace('.', ''))
-        savedpercent = saved / 100
+        savedInput = input()
 
-        # Converts the desired savings goal string into a float
         print("Enter desired retirement savings goal. (ex. $200,000)")
-        savingsgoalinput = input()
-        savingsgoalstr = ''
-        for x in range(1, len(savingsgoalinput)):
-            savingsgoalstr += savingsgoalinput[x]
-        savingsgoal = float(savingsgoalstr.replace(',',''))
-        #print(savingsgoal)
+        savingsGoalInput = input()
 
-        # Calculation for meeting savings goal
-        savingsperyear = salary * savedpercent
-        employermatch = 0.35 * savingsperyear
-        years = 1
-        savings = savingsperyear + employermatch
-
-        while savings <= savingsgoal:
-            savings += savingsperyear + employermatch
-            years += 1
-
-        savingsgoalage = years + age
-
-        if(savingsgoalage < 100):
-            print("You will reach your savings goal at age", savingsgoalage)
-        else:
-            print("You will reach your savings goal after 100 and will not meet it.")
+        retirement(age, salaryInput, savedInput, savingsGoalInput)
 
     elif(function == 3):
         print("Shortest Distance function selected.")
-        print("Input your 2 points.")
+        print("Input your 2 points. Format: (x1, y1), (x2, y2)")
         x1 = float(input("x1: "))
         y1 = float(input("y1: "))
         x2 = float(input("x2: "))
         y2 = float(input("y2: "))
-
-        xsquared = (x2 - x1) * (x2 - x1)
-        ysquared = (y2 - y1) * (y2 - y1)
-        inside = xsquared + ysquared
-        distance = math.sqrt(inside)
-
-        print("Distance between", (x1,y1), "and" , (x2,y2), "is", distance)
-
+        shortestDistance(x1, y1, x2, y2)
 
     elif(function == 4):
         print("Email Verifier function selected.")
         print("Enter your email.")
         emailstring = input()
-        # Checks if the first character of the email is an alphabet
-        z = 1
-        if(emailstring[0].isalpha() == False):
-            z = 0
-
-        # Checks if there is an instance of two periods next to each other
-        for x in range(0, len(emailstring) - 1):
-            if(emailstring[x] == '.'):
-                if(emailstring[x+1] == '.'):
-                    z = 0
-
-        # Checks for forbidden characters in the email string
-        if('""' in emailstring) or ('(' in emailstring) or (')' in emailstring) or (',' in emailstring) or (':' in emailstring) or (';' in emailstring) or ('<' in emailstring) or ('>' in emailstring) or ('[' in emailstring) or (']' in emailstring) or ('`' in emailstring) or ('\\' in emailstring):
-            z = 0
-        if(emailstring.count('@') > 1 or emailstring.count('@') == 0):
-            z = 0
-
-        # Checks to see if the '@' used in the string is for the @something.com
-        if('@' in emailstring):
-                # If after the @, there is no domain ( @.com)
-                if((len(emailstring) - emailstring.index('@')) == 5):
-                    z = 0
-                # If after the @, there is an non-alphabetical letter
-                if(emailstring[emailstring.index('@') + 1].isalpha() == False):
-                    z = 0
-                #print(x,"char:", emailstring[x])
-                if(emailstring[len(emailstring) - 1] != 'm'):
-                    z = 0
-                if(emailstring[len(emailstring) - 2] != 'o'):
-                    z = 0
-                if(emailstring[len(emailstring) - 3] != 'c'):
-                    z = 0
-                if(emailstring[len(emailstring) - 4] != '.'):
-                    z = 0
-                # If all the previous tests pass, it checks to make sure the domain are letters
-                #if(z == 1):
-                #    for x in range(emailstring.index('@')+1, emailstring.index('.')):
-                        #print(emailstring[x])
-                #        if(emailstring[x].isalpha() == False):
-                #            z = 0
-
-        # If all the tests pass, print out valid email
-        if(z == 1):
-            print("\nCongratulations, you have a valid email address!")
-        elif(z == 0):
-            print("\nInvalid email.")
+        email(emailstring)
 
     elif(function == 5):
         sys.exit(0)
